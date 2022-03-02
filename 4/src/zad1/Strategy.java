2e0ef4d60a9d16e34634da631ad74f1d94db0c65
ssagata <75955993+ssagata@users.noreply.github.com>
2022-03-02 13:22:59 +0100

    Delete 4 directory

--- 4/src/zad1/Strategy.java
+++
@@ -1,5 +0,0 @@
-package zad1;
-
-public interface Strategy {
-    public double sort(int []tab);
-}
--- 4/src/zad5/MyException.java
+++
@@ -1,7 +0,0 @@
-package zad5;
-
-public class MyException extends Exception{
-    public MyException( ){
-        System.out.println("There have to be two different numbers");
-    }
-}
